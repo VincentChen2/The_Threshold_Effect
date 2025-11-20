c82e34ca134502918f2b240c91a378945a395376
VincentChen2 <chen.vincent.japan@gmail.com>
2025-11-19 21:45:24 -0500

    Implemented Graph Generation

---
+++ src/main/java/org/Graph.java
@@ -0,0 +1,76 @@
+package org;
+
+import java.util.*;
+
+public class Graph {
+    private final int n;
+    private final double p;
+    private final Random random;
+    private final Map<Integer, Set<Integer>> adjacencyList;
+
+    public Graph(int n, double p) {
+        this.n = n;
+        this.p = p;
+        this.random = new Random();
+        this.adjacencyList = new HashMap<>(n);
+
+        generateGraph();
+    }
+
+    private void generateGraph() {
+        //Initialize all vertices
+        for (int i = 0; i < n; i++) {
+            adjacencyList.put(i, new HashSet<>());
+        }
+
+        //Generate edges
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (random.nextDouble() < p) {
+                    adjacencyList.get(i).add(j);
+                    adjacencyList.get(j).add(i);
+                }
+            }
+        }
+    }
+
+    public boolean hasEdge(int u, int v) {
+        return adjacencyList.get(u).contains(v);
+    }
+
+    public Set<Integer> getNeighbors(int vertex) {
+        if (vertex < 0 || vertex >= n) {
+            throw new IllegalArgumentException("Vertex index out of bounds: " + vertex);
+        }
+
+        return new HashSet<>(adjacencyList.get(vertex));
+    }
+
+    public int getVertexCount() {
+        return n;
+    }
+
+    public double getProbability() {
+        return p;
+    }
+
+    public int getEdgeCount() {
+        int edges = 0;
+
+        for (Set<Integer> neighbors : adjacencyList.values()) {
+            edges += neighbors.size();
+        }
+
+        return edges / 2;
+    }
+
+    public int getDegree(int vertex) {
+        return adjacencyList.get(vertex).size();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Graph[n=%d, p=%.3f, edges=%d]",
+                n, p, getEdgeCount());
+    }
+}
---
+++ src/main/java/org/Main.java
@@ -0,0 +1,8 @@
+package org;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph(10, 0.5);
+        System.out.println(graph);
+    }
+}
